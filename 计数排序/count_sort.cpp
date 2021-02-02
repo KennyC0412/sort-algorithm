c42fcab8aa1c020ef17b7cd1dd3f43c590c04080
KennyC0412 <377545253@qq.com>
2021-02-02 16:48:59 +0800

    修改为可以对某个范围内的数组排序

--- 计数排序/count_sort.cpp
+++ 计数排序/count_sort.cpp
@@ -3,17 +3,17 @@
 #include <iostream>
 #include <algorithm>
 
-std::vector<int> countSort(std::vector<int> &data,int max)
+std::vector<int> countSort(std::vector<int> &data,int max,int min)
 {
 	int length = data.size();
 	std::vector<int> ret(length);
-	std::vector<int> count(max,0);
+	std::vector<int> count(max-min,0);
 	for (int i = 0; i < length; ++i) {
-		count[data[i]]++;
+		count[data[i]-min]++;
 	}
 	for (int i = 0,j = 0; j < count.size(); ++j) {
 		while (count[j]--) {
-			ret[i++] = j;
+			ret[i++] = j+min;
 		}
 	}
 	return ret;
@@ -23,11 +23,12 @@ int main()
 {
 	std::vector<int> data;
 	int max = 100;
+	int min = 40;
 	std::srand(time(0));
-	for (int i = 0; i < 100; ++i) {
-		data.push_back(rand() % max);
+	for (int i = 0; i < 200; ++i) {
+		data.push_back(rand() % (max-min)+min);//²úÉú·¶Î§Îª[min,max)
 	}
-	data = countSort(data,max);
+	data = countSort(data,max,min);
 	std::for_each(data.begin(), data.end(), [](int data) { std::cout << data << ' '; });
 	return 0;
 }
