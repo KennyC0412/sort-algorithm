e5c7ba1d6793324a062ea89f9f6f52b723cd9f4d
KennyC0412 <377545253@qq.com>
2021-02-02 17:18:42 +0800

    修改为稳定排序

--- 计数排序/count_sort.cpp
+++ 计数排序/count_sort.cpp
@@ -11,10 +11,13 @@ std::vector<int> countSort(std::vector<int> &data,int max,int min)
 	for (int i = 0; i < length; ++i) {
 		count[data[i]-min]++;
 	}
-	for (int i = 0,j = 0; j < count.size(); ++j) {
-		while (count[j]--) {
-			ret[i++] = j+min;
-		}
+	//使用累加数组记录每一个元素最后的位置
+	for (int i = 1; i < max - min; ++i) {
+		count[i] += count[i - 1];
+	}
+	//对原数组中的每一个元素，倒序遍历，从count中查找其对应位置，保证排序稳定
+	for (int i = length - 1; i >= 0; --i) {
+		ret[--count[data[i]-min]] = data[i];
 	}
 	return ret;
 }
@@ -25,7 +28,7 @@ int main()
 	int max = 100;
 	int min = 40;
 	std::srand(time(0));
-	for (int i = 0; i < 200; ++i) {
+	for (int i = 0; i < 100; ++i) {
 		data.push_back(rand() % (max-min)+min);//产生范围为[min,max)
 	}
 	data = countSort(data,max,min);
